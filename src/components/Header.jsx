371e496a8c8114db41f5618110ac5ae816e1d4b6
Lucas Siqueira <lucasvinicius.siqueira@gmail.com>
2023-04-11 23:51:01 -0300

    refactor: rearranging elements in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import logoTrybeWallet from '../assets/svg/logo-trybewallet.svg';
+import coinsIcon from '../assets/svg/coins-icon.svg';
+import profileIcon from '../assets/svg/profile-icon.svg';
 
 class Header extends Component {
   render() {
     const { email } = this.props;
     return (
       <header>
-        <div>Header</div>
-        <p data-testid="email-field">{email}</p>
-        <p data-testid="total-field">0</p>
-        <p data-testid="header-currency-field">BRL</p>
+        <img src={ logoTrybeWallet } alt="Logo da TrybeWallet" />
+        <div>
+          <img src={ coinsIcon } alt="Icone de moedas" />
+          <span>Total de despesas:</span>
+          <span data-testid="total-field">0</span>
+          <span data-testid="header-currency-field">BRL</span>
+        </div>
+        <div>
+          <img src={ profileIcon } alt="Icone de perfil" />
+          <span data-testid="email-field">{email}</span>
+        </div>
       </header>
     );
   }
